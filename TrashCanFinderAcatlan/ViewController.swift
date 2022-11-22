b0fa2c6babc40ff9f16a846b881cd457f7819932
IOS LAB Acatlan <ioslabacatlan@IOSs-MacBook-Pro.local>
2022-11-22 11:03:57 -0600

    proyecto inicializado

---
+++ TrashCanFinderAcatlan/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TrashCanFinderAcatlan
+//
+//  Created by IOS LAB Acatlan on 22/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
