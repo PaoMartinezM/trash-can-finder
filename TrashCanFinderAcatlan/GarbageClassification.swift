c3067ad64b22c195b584a608a96e7c7d45437f33
IOS LAB Acatlan <ioslabacatlan@IOSs-MacBook-Pro.local>
2022-11-23 10:00:14 -0600

    paso intermedio

--- TrashCanFinderAcatlan/GarbageClassification.swift
+++ TrashCanFinderAcatlan/GarbageClassification.swift
@@ -15,14 +15,15 @@ class GarbageClassification {
     
     static let classifications = [
         GarbageClassification(key: "0", readableName: "Colillas", trashPoints: 5),
-        GarbageClassification(key: "1", readableName: "Papel/Cartón", trashPoints: 10),
-        GarbageClassification(key: "2", readableName: "Pet", trashPoints: 10),
-        GarbageClassification(key: "3", readableName: "Orgánico", trashPoints: 10),
-        GarbageClassification(key: "4", readableName: "Metal", trashPoints: 50),
-        GarbageClassification(key: "5", readableName: "Pilas", trashPoints: 50),
-        GarbageClassification(key: "6", readableName: "Vidrio", trashPoints: 50),
-        GarbageClassification(key: "7", readableName: "Electrónicos", trashPoints: 100),
-        GarbageClassification(key: "8", readableName: "Otros", trashPoints: 5)
+        GarbageClassification(key: "1", readableName: "Papel", trashPoints: 10),
+        GarbageClassification(key: "2", readableName: "Cartón", trashPoints: 10),
+        GarbageClassification(key: "3", readableName: "Pet", trashPoints: 10),
+        GarbageClassification(key: "4", readableName: "Orgánico", trashPoints: 10),
+        GarbageClassification(key: "5", readableName: "Latas", trashPoints: 50),
+        GarbageClassification(key: "6", readableName: "Pilas", trashPoints: 50),
+        GarbageClassification(key: "7", readableName: "Vidrio", trashPoints: 50),
+        GarbageClassification(key: "8", readableName: "Electrónicos", trashPoints: 100),
+        GarbageClassification(key: "9", readableName: "Otros", trashPoints: 5)
     ]
     
     init(key: String, readableName: String, trashPoints: Int) {
